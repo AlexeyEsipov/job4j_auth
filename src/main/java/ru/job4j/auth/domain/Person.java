5e5b46f6011f0468ba07fcdf970628781fb7dffe
Esipov Aleksey <a_esipov@list.ru>
2022-11-01 14:28:28 +0300

    1. RESTFull. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/auth/domain/Person.java
@@ -0,0 +1,21 @@
+package ru.job4j.auth.domain;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+}
---
+++ src/main/resources/db/001_ddl_create_schema.sql
@@ -0,0 +1,5 @@
+create table person (
+    id serial primary key not null,
+    login varchar,
+    password varchar
+);
