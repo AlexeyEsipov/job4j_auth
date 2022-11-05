3ccace34244c4f78ffd0915a498e88c38276fd6b
Esipov Aleksey <a_esipov@list.ru>
2022-11-05 10:29:33 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -1,13 +1,22 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Job4jAuthApplication {
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Job4jAuthApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Job4jAuthApplication.class, args);
@@ -21,5 +30,13 @@ public class Job4jAuthApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
 	}
 }
